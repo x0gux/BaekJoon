a141652b0fcc93e3bb671ea4056e49ac807fbf81
x0gux <143389711+x0gux@users.noreply.github.com>
2026-02-24 22:18:01 +0900

    [Bronze V] Title: Roller Coaster Ride, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/34454. Roller Coaster Ride/Roller Coaster Ride.cc
@@ -0,0 +1,11 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int a,b,c;
+    scanf("%d %d %d",&a,&b,&c);
+    if( a <= b*c) printf("yes");
+    else printf("no");
+
+    return 0;
+}
